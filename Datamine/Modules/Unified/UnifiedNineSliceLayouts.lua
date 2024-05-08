c3f7805e95e35d0c04888aadf38e26f6c576b58d
Ghostopheles <ghost@kivatech.io>
2024-05-08 14:42:39 -0500

    Edit Nineslices

--- Datamine/Modules/Unified/UnifiedNineSliceLayouts.lua
+++ Datamine/Modules/Unified/UnifiedNineSliceLayouts.lua
@@ -1,74 +1,100 @@
-NineSliceUtil.AddLayout("DatamineCorruptedBorder", {
-    TopLeftCorner =	{
-        atlas = "Tooltip-Corrupted-NineSlice-CornerTopLeft",
-    },
-    TopRightCorner = {
-        atlas = "Tooltip-Corrupted-NineSlice-CornerTopRight",
-    },
-    BottomLeftCorner = {
-        atlas = "Tooltip-Corrupted-NineSlice-CornerBottomLeft",
-    },
-    BottomRightCorner =	{
-        atlas = "Tooltip-Corrupted-NineSlice-CornerBottomRight",
-    },
-    TopEdge = {
-        atlas = "_Tooltip-Corrupted-NineSlice-EdgeTop",
-    },
-    BottomEdge = {
-        atlas = "_Tooltip-Corrupted-NineSlice-EdgeBottom",
-    },
-    LeftEdge = {
-        atlas = "!Tooltip-Corrupted-NineSlice-EdgeLeft",
-    },
-    RightEdge = {
-        atlas = "!Tooltip-Corrupted-NineSlice-EdgeRight",
-    },
-});
+do
+    NineSliceUtil.AddLayout("DatamineCorruptedBorder", {
+        TopLeftCorner =	{
+            atlas = "Tooltip-Corrupted-NineSlice-CornerTopLeft",
+        },
+        TopRightCorner = {
+            atlas = "Tooltip-Corrupted-NineSlice-CornerTopRight",
+        },
+        BottomLeftCorner = {
+            atlas = "Tooltip-Corrupted-NineSlice-CornerBottomLeft",
+        },
+        BottomRightCorner =	{
+            atlas = "Tooltip-Corrupted-NineSlice-CornerBottomRight",
+        },
+        TopEdge = {
+            atlas = "_Tooltip-Corrupted-NineSlice-EdgeTop",
+        },
+        BottomEdge = {
+            atlas = "_Tooltip-Corrupted-NineSlice-EdgeBottom",
+        },
+        LeftEdge = {
+            atlas = "!Tooltip-Corrupted-NineSlice-EdgeLeft",
+        },
+        RightEdge = {
+            atlas = "!Tooltip-Corrupted-NineSlice-EdgeRight",
+        },
+    });
+end
 
-local bCornerOffset = 4;
-local bCenterOffset = 8;
-NineSliceUtil.AddLayout("DatamineBlueHighlight", {
-    mirrorLayout = true,
-    TopLeftCorner =	{
-        atlas = "editmode-actionbar-highlight-nineslice-corner",
-        x = -bCornerOffset,
-        y = bCornerOffset,
-    },
-    TopRightCorner = {
-        atlas = "editmode-actionbar-highlight-nineslice-corner",
-        x = bCornerOffset,
-        y = bCornerOffset,
-    },
-    BottomLeftCorner = {
-        atlas = "editmode-actionbar-highlight-nineslice-corner",
-        x = -bCornerOffset,
-        y = -bCornerOffset,
-    },
-    BottomRightCorner = {
-        atlas = "editmode-actionbar-highlight-nineslice-corner",
-        x = bCornerOffset,
-        y = -bCornerOffset,
-    },
-    TopEdge = {
-        atlas = "_editmode-actionbar-highlight-nineslice-edgetop",
-    },
-    BottomEdge = {
-        atlas = "_editmode-actionbar-highlight-nineslice-edgebottom",
-        mirrorLayout = false,
-    },
-    LeftEdge = {
-        atlas = "!editmode-actionbar-highlight-nineslice-edgeleft",
-        mirrorLayout = false,
-    },
-    RightEdge = {
-        atlas = "!editmode-actionbar-highlight-nineslice-edgeright",
-        mirrorLayout = false,
-    },
-    Center = {
-        atlas = "editmode-actionbar-highlight-nineslice-center",
-        x = -bCenterOffset,
-        y = bCenterOffset,
-        x1 = bCenterOffset,
-        y1 = -bCenterOffset
-    },
-});
+do
+    local bCornerOffset = 4;
+    local bCenterOffset = 8;
+    NineSliceUtil.AddLayout("DatamineBlueHighlight", {
+        mirrorLayout = true,
+        TopLeftCorner =	{
+            atlas = "editmode-actionbar-highlight-nineslice-corner",
+            x = -bCornerOffset,
+            y = bCornerOffset,
+        },
+        TopRightCorner = {
+            atlas = "editmode-actionbar-highlight-nineslice-corner",
+            x = bCornerOffset,
+            y = bCornerOffset,
+        },
+        BottomLeftCorner = {
+            atlas = "editmode-actionbar-highlight-nineslice-corner",
+            x = -bCornerOffset,
+            y = -bCornerOffset,
+        },
+        BottomRightCorner = {
+            atlas = "editmode-actionbar-highlight-nineslice-corner",
+            x = bCornerOffset,
+            y = -bCornerOffset,
+        },
+        TopEdge = {
+            atlas = "_editmode-actionbar-highlight-nineslice-edgetop",
+        },
+        BottomEdge = {
+            atlas = "_editmode-actionbar-highlight-nineslice-edgebottom",
+            mirrorLayout = false,
+        },
+        LeftEdge = {
+            atlas = "!editmode-actionbar-highlight-nineslice-edgeleft",
+            mirrorLayout = false,
+        },
+        RightEdge = {
+            atlas = "!editmode-actionbar-highlight-nineslice-edgeright",
+            mirrorLayout = false,
+        },
+        Center = {
+            atlas = "editmode-actionbar-highlight-nineslice-center",
+            x = -bCenterOffset,
+            y = bCenterOffset,
+            x1 = bCenterOffset,
+            y1 = -bCenterOffset
+        },
+    });
+end
+
+do
+    NineSliceUtil.AddLayout("DatamineDottedSelectionHighlight", {
+        mirrorLayout = true,
+        threeSliceHorizontal = true,
+        TopEdge = {
+            atlas = "NineSliceTool-DottedLine-Horizontal",
+            mirrorLayout = false,
+        },
+        BottomEdge = {
+            atlas = "NineSliceTool-DottedLine-Horizontal",
+        },
+        LeftEdge = {
+            atlas = "NineSliceTool-DottedLine-Vertical",
+            mirrorLayout = false,
+        },
+        RightEdge = {
+            atlas = "NineSliceTool-DottedLine-Vertical",
+        },
+    });
+end
+
