327e0bc74bab46bfba68eb73ce61fd24cda0ed30
Ghostopheles <ghost@kivatech.io>
2024-06-17 16:57:28 -0500

    Change default tooltip colors again

--- Datamine/Modules/Tooltips/Settings.lua
+++ Datamine/Modules/Tooltips/Settings.lua
@@ -7,7 +7,7 @@ local category = Settings.RegisterVerticalLayoutSubcategory(parentCategory, L.CO
 do
     local variable = "TooltipKeyColor";
     local name = L.CONFIG_TOOLTIP_KEY_COLOR_NAME;
-    local default = DatamineLightGray:GenerateHexColor();
+    local default = "FFBFBCE0";
     local setting = S.RegisterSetting(category, variable, name, default);
     local tooltip = L.CONFIG_TOOLTIP_KEY_COLOR_TOOLTIP;
 
@@ -17,7 +17,7 @@ end
 do
     local variable = "TooltipValueColor";
     local name = L.CONFIG_TOOLTIP_VALUE_COLOR_NAME;
-    local default = DatamineDarkGray:GenerateHexColor();
+    local default = "FFFFC16A";
     local setting = S.RegisterSetting(category, variable, name, default);
     local tooltip = L.CONFIG_TOOLTIP_VALUE_COLOR_TOOLTIP;
 
