0aa89891ef29550723385ae947e3b3137dfc7475
Ghostopheles <ghost@kivatech.io>
2024-06-17 15:37:15 -0500

    Optimization

--- Datamine/Modules/Tooltips/Settings.lua
+++ Datamine/Modules/Tooltips/Settings.lua
@@ -71,8 +71,8 @@ end
 local function RegisterSettingsTable(settings)
     for _, _setting in ipairs(settings) do
         local variable = _setting.Name;
-        local name = L[_setting.LocKey .. "_NAME"];
-        local tooltip = L[_setting.LocKey .. "_TOOLTIP"];
+        local name = L["CONFIG_TOOLTIP_SHOW_" .. _setting.LocKey .. "_NAME"];
+        local tooltip = L["CONFIG_TOOLTIP_SHOW_" .. _setting.LocKey .. "_TOOLTIP"];
         local default;
         if _setting.Default ~= nil then
             default = _setting.Default;
@@ -92,47 +92,47 @@ S.CreateHeader(category, L.CONFIG_HEADER_ITEM_TOOLTIPS);
 local itemSettings = {
     [1] = {
         Name = "TooltipItemShowItemID",
-        LocKey = "CONFIG_TOOLTIP_SHOW_ITEM_ID",
+        LocKey = "ITEM_ID",
     },
     [2] = {
         Name = "TooltipItemShowEnchantID",
-        LocKey = "CONFIG_TOOLTIP_SHOW_ITEM_ENCHANT_ID",
+        LocKey = "ITEM_ENCHANT_ID",
     },
     [3] = {
         Name = "TooltipItemShowGemIDs",
-        LocKey = "CONFIG_TOOLTIP_SHOW_ITEM_GEMS",
+        LocKey = "ITEM_GEMS",
     },
     [4] = {
         Name = "TooltipItemShowItemContext",
-        LocKey = "CONFIG_TOOLTIP_SHOW_ITEM_CONTEXT",
+        LocKey = "ITEM_CONTEXT",
         Default = false,
     },
     [5] = {
         Name = "TooltipItemShowBonusIDs",
-        LocKey = "CONFIG_TOOLTIP_SHOW_ITEM_BONUSES",
+        LocKey = "ITEM_BONUSES",
     },
     [6] = {
         Name = "TooltipItemShowModifiers",
-        LocKey = "CONFIG_TOOLTIP_SHOW_ITEM_MODIFIERS",
+        LocKey = "ITEM_MODIFIERS",
         Default = false,
     },
     [7] = {
         Name = "TooltipItemShowCrafterGUID",
-        LocKey = "CONFIG_TOOLTIP_SHOW_ITEM_CRAFTER_GUID",
+        LocKey = "ITEM_CRAFTER_GUID",
         Default = false,
     },
     [8] = {
         Name = "TooltipItemShowExtraEnchantID",
-        LocKey = "CONFIG_TOOLTIP_SHOW_ITEM_EXTRA_ENCHANT_ID",
+        LocKey = "ITEM_EXTRA_ENCHANT_ID",
         Default = false,
     },
     [9] = {
         Name = "TooltipItemShowItemSpellID",
-        LocKey = "CONFIG_TOOLTIP_SHOW_ITEM_SPELL",
+        LocKey = "ITEM_SPELL",
     },
     [10] = {
         Name = "TooltipItemShowRelicBonuses",
-        LocKey = "CONFIG_TOOLTIP_SHOW_ITEM_RELIC_BONUSES",
+        LocKey = "ITEM_RELIC_BONUSES",
         Default = false,
     }
 };
@@ -146,7 +146,7 @@ S.CreateHeader(category, L.CONFIG_HEADER_SPELL_TOOLTIPS);
 local spellSettings = {
     [1] = {
         Name = "TooltipSpellShowSpellID",
-        LocKey = "CONFIG_TOOLTIP_SHOW_SPELL_ID",
+        LocKey = "SPELL_ID",
     }
 };
 
@@ -159,15 +159,15 @@ S.CreateHeader(category, L.CONFIG_HEADER_MACRO_TOOLTIPS);
 local macroSettings = {
     [1] = {
         Name = "TooltipMacroShowMacroName",
-        LocKey = "CONFIG_TOOLTIP_SHOW_MACRO",
+        LocKey = "MACRO",
     },
     [2] = {
         Name = "TooltipMacroShowMacroAction",
-        LocKey = "CONFIG_TOOLTIP_SHOW_MACRO_ACTION",
+        LocKey = "MACRO_ACTION",
     },
     [3] = {
         Name = "TooltipMacroShowMacroIcon",
-        LocKey = "CONFIG_TOOLTIP_SHOW_MACRO_ICON",
+        LocKey = "MACRO_ICON",
     },
 };
 
@@ -180,15 +180,15 @@ S.CreateHeader(category, L.CONFIG_HEADER_TOY_TOOLTIPS);
 local toySettings = {
     [1] = {
         Name = "TooltipToyShowItemID",
-        LocKey = "CONFIG_TOOLTIP_SHOW_ITEM_ID",
+        LocKey = "ITEM_ID",
     },
     [2] = {
         Name = "TooltipToyShowItemSpell",
-        LocKey = "CONFIG_TOOLTIP_SHOW_ITEM_SPELL",
+        LocKey = "ITEM_SPELL",
     },
     [3] = {
         Name = "TooltipToyShowIcon",
-        LocKey = "CONFIG_TOOLTIP_SHOW_TOY_ICON",
+        LocKey = "TOY_ICON",
     },
 };
 
@@ -201,47 +201,47 @@ S.CreateHeader(category, L.CONFIG_HEADER_MOUNT_TOOLTIPS);
 local mountSettings = {
     [1] = {
         Name = "TooltipMountShowMountID",
-        LocKey = "CONFIG_TOOLTIP_SHOW_MOUNT_ID",
+        LocKey = "MOUNT_ID",
     },
     [2] = {
         Name = "TooltipMountShowSpellID",
-        LocKey = "CONFIG_TOOLTIP_SHOW_MOUNT_SPELL",
+        LocKey = "MOUNT_SPELL",
     },
     [3] = {
         Name = "TooltipMountShowIcon",
-        LocKey = "CONFIG_TOOLTIP_SHOW_MOUNT_ICON",
+        LocKey = "MOUNT_ICON",
     },
     [4] = {
         Name = "TooltipMountShowFaction",
-        LocKey = "CONFIG_TOOLTIP_SHOW_MOUNT_FACTION",
+        LocKey = "MOUNT_FACTION",
     },
     [5] = {
         Name = "TooltipMountShowSkyriding",
-        LocKey = "CONFIG_TOOLTIP_SHOW_MOUNT_SKYRIDING",
+        LocKey = "MOUNT_SKYRIDING",
     },
     [6] = {
         Name = "TooltipMountShowDisplay",
-        LocKey = "CONFIG_TOOLTIP_SHOW_MOUNT_DISPLAY",
+        LocKey = "MOUNT_DISPLAY",
         Default = false,
     },
     [7] = {
         Name = "TooltipMountShowType",
-        LocKey = "CONFIG_TOOLTIP_SHOW_MOUNT_TYPE",
+        LocKey = "MOUNT_TYPE",
         Default = false,
     },
     [8] = {
         Name = "TooltipMountShowModelScene",
-        LocKey = "CONFIG_TOOLTIP_SHOW_MOUNT_MODELSCENE",
+        LocKey = "MOUNT_MODELSCENE",
         Default = false,
     },
     [9] = {
         Name = "TooltipMountShowAnim",
-        LocKey = "CONFIG_TOOLTIP_SHOW_MOUNT_ANIM",
+        LocKey = "MOUNT_ANIM",
         Default = false,
     },
     [10] = {
         Name = "TooltipMountShowSpellVisual",
-        LocKey = "CONFIG_TOOLTIP_SHOW_MOUNT_SPELLVISUAL",
+        LocKey = "MOUNT_SPELLVISUAL",
         Default = false,
     },
 };
@@ -255,19 +255,19 @@ S.CreateHeader(category, L.CONFIG_HEADER_UNIT_TOOLTIPS);
 local unitSettings = {
     [1] = {
         Name = "TooltipUnitShowUnitToken",
-        LocKey = "CONFIG_TOOLTIP_SHOW_UNIT_TOKEN",
+        LocKey = "UNIT_TOKEN",
     },
     [2] = {
         Name = "TooltipUnitShowCreatureID",
-        LocKey = "CONFIG_TOOLTIP_SHOW_UNIT_CREATURE_ID",
+        LocKey = "UNIT_CREATURE_ID",
     },
     [3] = {
         Name = "TooltipUnitShowDisplayID",
-        LocKey = "CONFIG_TOOLTIP_SHOW_UNIT_DISPLAY_ID",
+        LocKey = "UNIT_DISPLAY_ID",
     },
     [4] = {
         Name = "TooltipUnitShowNPCClass",
-        LocKey = "CONFIG_TOOLTIP_SHOW_UNIT_NPC_CLASS",
+        LocKey = "UNIT_NPC_CLASS",
         Default = false,
     }
 };
@@ -281,68 +281,68 @@ S.CreateHeader(category, L.CONFIG_HEADER_AURA_TOOLTIPS);
 local auraSettings = {
     [1] = {
         Name = "TooltipAuraShowSpellID",
-        LocKey = "CONFIG_TOOLTIP_SHOW_AURA_ID",
+        LocKey = "AURA_ID",
     },
     [2] = {
         Name = "TooltipAuraShowIcon",
-        LocKey = "CONFIG_TOOLTIP_SHOW_AURA_ICON",
+        LocKey = "AURA_ICON",
     },
     [3] = {
         Name = "TooltipAuraShowDispel",
-        LocKey = "CONFIG_TOOLTIP_SHOW_AURA_DISPEL",
+        LocKey = "AURA_DISPEL",
         Default = false,
     },
     [4] = {
         Name = "TooltipAuraShowIsBossAura",
-        LocKey = "CONFIG_TOOLTIP_SHOW_AURA_BOSS_AURA",
+        LocKey = "AURA_BOSS_AURA",
         Default = false,
     },
     [5] = {
         Name = "TooltipAuraShowCharges",
-        LocKey = "CONFIG_TOOLTIP_SHOW_AURA_CHARGES",
+        LocKey = "AURA_CHARGES",
         Default = false,
     },
     [6] = {
         Name = "TooltipAuraShowMaxCharges",
-        LocKey = "CONFIG_TOOLTIP_SHOW_AURA_MAX_CHARGES",
+        LocKey = "AURA_MAX_CHARGES",
         Default = false,
     },
     [7] = {
         Name = "TooltipAuraShowSourceUnit",
-        LocKey = "CONFIG_TOOLTIP_SHOW_AURA_SOURCE_UNIT",
+        LocKey = "AURA_SOURCE_UNIT",
     },
     [8] = {
         Name = "TooltipAuraShowSourceName",
-        LocKey = "CONFIG_TOOLTIP_SHOW_AURA_SOURCE",
+        LocKey = "AURA_SOURCE",
         Default = false,
     },
     [9] = {
         Name = "TooltipAuraShowInstanceID",
-        LocKey = "CONFIG_TOOLTIP_SHOW_AURA_INSTANCE_ID",
+        LocKey = "AURA_INSTANCE_ID",
     },
     [10] = {
         Name = "TooltipAuraShowStacks",
-        LocKey = "CONFIG_TOOLTIP_SHOW_AURA_STACKS",
+        LocKey = "AURA_STACKS",
         Default = false,
     },
     [11] = {
         Name = "TooltipAuraShowPlayerApplicable",
-        LocKey = "CONFIG_TOOLTIP_SHOW_AURA_PLAYER_APPLICABLE",
+        LocKey = "AURA_PLAYER_APPLICABLE",
         Default = false,
     },
     [12] = {
         Name = "TooltipAuraShowFromPlayerOrPet",
-        LocKey = "CONFIG_TOOLTIP_SHOW_AURA_FROM_PLAYER_OR_PET",
+        LocKey = "AURA_FROM_PLAYER_OR_PET",
         Default = false,
     },
     [13] = {
         Name = "TooltipAuraShowPoints",
-        LocKey = "CONFIG_TOOLTIP_SHOW_AURA_POINTS",
+        LocKey = "AURA_POINTS",
         Default = false,
     },
     [14] = {
         Name = "TooltipAuraShowIsPrivate",
-        LocKey = "CONFIG_TOOLTIP_SHOW_AURA_IS_PRIVATE",
+        LocKey = "AURA_IS_PRIVATE",
         Default = false,
     },
 };
@@ -356,24 +356,24 @@ S.CreateHeader(category, L.CONFIG_HEADER_ACHIEVEMENT_TOOLTIPS);
 local achievementSettings = {
     [1] = {
         Name = "TooltipAchievementShowID",
-        LocKey = "CONFIG_TOOLTIP_SHOW_ACHIEVEMENT_ID",
+        LocKey = "ACHIEVEMENT_ID",
     },
     [2] = {
         Name = "TooltipAchievementShowPlayerGUID",
-        LocKey = "CONFIG_TOOLTIP_SHOW_ACHIEVEMENT_PLAYER_GUID",
+        LocKey = "ACHIEVEMENT_PLAYER_GUID",
     },
     [3] = {
         Name = "TooltipAchievementShowCompleted",
-        LocKey = "CONFIG_TOOLTIP_SHOW_ACHIEVEMENT_COMPLETED",
+        LocKey = "ACHIEVEMENT_COMPLETED",
     },
     [4] = {
         Name = "TooltipAchievementShowDate",
-        LocKey = "CONFIG_TOOLTIP_SHOW_ACHIEVEMENT_DATE",
+        LocKey = "ACHIEVEMENT_DATE",
         Default = false,
     },
     [5] = {
         Name = "TooltipAchievementShowCriteria",
-        LocKey = "CONFIG_TOOLTIP_SHOW_ACHIEVEMENT_CRITERIA",
+        LocKey = "ACHIEVEMENT_CRITERIA",
         Default = false,
     },
 };
