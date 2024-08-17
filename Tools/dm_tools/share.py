aa1221b9826c310543eb40a772e0650f838a4a5b
Ghostopheles <ghost@ghst.tools>
2024-08-17 06:46:19 -0500

    `get_latest_build` function

--- Tools/dm_tools/share.py
+++ Tools/dm_tools/share.py
@@ -1,3 +1,5 @@
+import httpx
+
 from os.path import dirname, realpath, join
 
 from dm_logging import get_logger
@@ -7,3 +9,12 @@ SELF_PATH = dirname(realpath(__file__))
 CACHE_PATH = join(SELF_PATH, "cache")
 
 logger = get_logger()
+
+
+def get_latest_build() -> str:
+    with httpx.Client(http2=True, headers=SHARED_CLIENT_HEADERS) as client:
+        res = client.get("https://wago.tools/api/builds/wow/latest")
+        res.raise_for_status()
+
+    data = res.json()
+    return data["version"]
