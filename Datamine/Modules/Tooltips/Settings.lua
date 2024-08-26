9eb90a33fdc56db74ba7fa4c7fe97aaf437ec68d
Ghostopheles <ghost@ghst.tools>
2024-08-26 01:25:22 -0500

    Clean up the streets

--- Datamine/Modules/Tooltips/Settings.lua
+++ Datamine/Modules/Tooltips/Settings.lua
@@ -177,7 +177,7 @@ RegisterSettingsTable(itemSettings, items);
 local spells = CreateSubcategory(L.CONFIG_HEADER_SPELL_TOOLTIPS);
 
 local spellSettings = {
-    [1] = {
+    {
         Name = "TooltipSpellShowSpellID",
         LocKey = "SPELL_ID",
     }
@@ -190,15 +190,15 @@ RegisterSettingsTable(spellSettings, spells);
 local macros = CreateSubcategory(L.CONFIG_HEADER_MACRO_TOOLTIPS);
 
 local macroSettings = {
-    [1] = {
+    {
         Name = "TooltipMacroShowMacroName",
         LocKey = "MACRO",
     },
-    [2] = {
+    {
         Name = "TooltipMacroShowMacroAction",
         LocKey = "MACRO_ACTION",
     },
-    [3] = {
+    {
         Name = "TooltipMacroShowMacroIcon",
         LocKey = "MACRO_ICON",
     },
@@ -211,15 +211,15 @@ RegisterSettingsTable(macroSettings, macros);
 local toys = CreateSubcategory(L.CONFIG_HEADER_TOY_TOOLTIPS);
 
 local toySettings = {
-    [1] = {
+    {
         Name = "TooltipToyShowItemID",
         LocKey = "ITEM_ID",
     },
-    [2] = {
+    {
         Name = "TooltipToyShowItemSpell",
         LocKey = "ITEM_SPELL",
     },
-    [3] = {
+    {
         Name = "TooltipToyShowIcon",
         LocKey = "TOY_ICON",
     },
@@ -232,47 +232,47 @@ RegisterSettingsTable(toySettings, toys);
 local mounts = CreateSubcategory(L.CONFIG_HEADER_MOUNT_TOOLTIPS);
 
 local mountSettings = {
-    [1] = {
+    {
         Name = "TooltipMountShowMountID",
         LocKey = "MOUNT_ID",
     },
-    [2] = {
+    {
         Name = "TooltipMountShowSpellID",
         LocKey = "MOUNT_SPELL",
     },
-    [3] = {
+    {
         Name = "TooltipMountShowIcon",
         LocKey = "MOUNT_ICON",
     },
-    [4] = {
+    {
         Name = "TooltipMountShowFaction",
         LocKey = "MOUNT_FACTION",
     },
-    [5] = {
+    {
         Name = "TooltipMountShowSkyriding",
         LocKey = "MOUNT_SKYRIDING",
     },
-    [6] = {
+    {
         Name = "TooltipMountShowDisplay",
         LocKey = "MOUNT_DISPLAY",
         Default = false,
     },
-    [7] = {
+    {
         Name = "TooltipMountShowType",
         LocKey = "MOUNT_TYPE",
         Default = false,
     },
-    [8] = {
+    {
         Name = "TooltipMountShowModelScene",
         LocKey = "MOUNT_MODELSCENE",
         Default = false,
     },
-    [9] = {
+    {
         Name = "TooltipMountShowAnim",
         LocKey = "MOUNT_ANIM",
         Default = false,
     },
-    [10] = {
+    {
         Name = "TooltipMountShowSpellVisual",
         LocKey = "MOUNT_SPELLVISUAL",
         Default = false,
@@ -286,19 +286,19 @@ RegisterSettingsTable(mountSettings, mounts);
 local units = CreateSubcategory(L.CONFIG_HEADER_UNIT_TOOLTIPS);
 
 local unitSettings = {
-    [1] = {
+    {
         Name = "TooltipUnitShowUnitToken",
         LocKey = "UNIT_TOKEN",
     },
-    [2] = {
+    {
         Name = "TooltipUnitShowCreatureID",
         LocKey = "UNIT_CREATURE_ID",
     },
-    [3] = {
+    {
         Name = "TooltipUnitShowDisplayID",
         LocKey = "UNIT_DISPLAY_ID",
     },
-    [4] = {
+    {
         Name = "TooltipUnitShowNPCClass",
         LocKey = "UNIT_NPC_CLASS",
         Default = false,
@@ -312,68 +312,68 @@ RegisterSettingsTable(unitSettings, units);
 local auras = CreateSubcategory(L.CONFIG_HEADER_AURA_TOOLTIPS);
 
 local auraSettings = {
-    [1] = {
+    {
         Name = "TooltipAuraShowSpellID",
         LocKey = "AURA_ID",
     },
-    [2] = {
+    {
         Name = "TooltipAuraShowIcon",
         LocKey = "AURA_ICON",
     },
-    [3] = {
+    {
         Name = "TooltipAuraShowDispel",
         LocKey = "AURA_DISPEL",
         Default = false,
     },
-    [4] = {
+    {
         Name = "TooltipAuraShowIsBossAura",
         LocKey = "AURA_BOSS_AURA",
         Default = false,
     },
-    [5] = {
+    {
         Name = "TooltipAuraShowCharges",
         LocKey = "AURA_CHARGES",
         Default = false,
     },
-    [6] = {
+    {
         Name = "TooltipAuraShowMaxCharges",
         LocKey = "AURA_MAX_CHARGES",
         Default = false,
     },
-    [7] = {
+    {
         Name = "TooltipAuraShowSourceUnit",
         LocKey = "AURA_SOURCE_UNIT",
     },
-    [8] = {
+    {
         Name = "TooltipAuraShowSourceName",
         LocKey = "AURA_SOURCE",
         Default = false,
     },
-    [9] = {
+    {
         Name = "TooltipAuraShowInstanceID",
         LocKey = "AURA_INSTANCE_ID",
     },
-    [10] = {
+    {
         Name = "TooltipAuraShowStacks",
         LocKey = "AURA_STACKS",
         Default = false,
     },
-    [11] = {
+    {
         Name = "TooltipAuraShowPlayerApplicable",
         LocKey = "AURA_PLAYER_APPLICABLE",
         Default = false,
     },
-    [12] = {
+    {
         Name = "TooltipAuraShowFromPlayerOrPet",
         LocKey = "AURA_FROM_PLAYER_OR_PET",
         Default = false,
     },
-    [13] = {
+    {
         Name = "TooltipAuraShowPoints",
         LocKey = "AURA_POINTS",
         Default = false,
     },
-    [14] = {
+    {
         Name = "TooltipAuraShowIsPrivate",
         LocKey = "AURA_IS_PRIVATE",
         Default = false,
@@ -387,24 +387,24 @@ RegisterSettingsTable(auraSettings, auras);
 local achievements = CreateSubcategory(L.CONFIG_HEADER_ACHIEVEMENT_TOOLTIPS);
 
 local achievementSettings = {
-    [1] = {
+    {
         Name = "TooltipAchievementShowID",
         LocKey = "ACHIEVEMENT_ID",
     },
-    [2] = {
+    {
         Name = "TooltipAchievementShowPlayerGUID",
         LocKey = "ACHIEVEMENT_PLAYER_GUID",
     },
-    [3] = {
+    {
         Name = "TooltipAchievementShowCompleted",
         LocKey = "ACHIEVEMENT_COMPLETED",
     },
-    [4] = {
+    {
         Name = "TooltipAchievementShowDate",
         LocKey = "ACHIEVEMENT_DATE",
         Default = false,
     },
-    [5] = {
+    {
         Name = "TooltipAchievementShowCriteria",
         LocKey = "ACHIEVEMENT_CRITERIA",
         Default = false,
@@ -418,41 +418,41 @@ RegisterSettingsTable(achievementSettings, achievements);
 local battlePets = CreateSubcategory(L.CONFIG_HEADER_BATTLE_PET_TOOLTIPS);
 
 local battlePetSettings = {
-    [1] = {
+    {
         Name = "TooltipBattlePetShowSpeciesID",
         LocKey = "BATTLE_PET_SPECIES",
     },
-    [2] = {
+    {
         Name = "TooltipBattlePetShowLevel",
         LocKey = "BATTLE_PET_LEVEL",
         Default = false,
     },
-    [3] = {
+    {
         Name = "TooltipBattlePetShowBreedQuality",
         LocKey = "BATTLE_PET_BREED_QUALITY",
         Default = false,
     },
-    [4] = {
+    {
         Name = "TooltipBattlePetShowMaxHealth",
         LocKey = "BATTLE_PET_MAX_HEALTH",
         Default = false,
     },
-    [5] = {
+    {
         Name = "TooltipBattlePetShowPower",
         LocKey = "BATTLE_PET_POWER",
         Default = false,
     },
-    [6] = {
+    {
         Name = "TooltipBattlePetShowSpeed",
         LocKey = "BATTLE_PET_SPEED",
         Default = false,
     },
-    [7] = {
+    {
         Name = "TooltipBattlePetShowPetID",
         LocKey = "BATTLE_PET_ID",
         Default = false,
     },
-    [8] = {
+    {
         Name = "TooltipBattlePetShowDisplayID",
         LocKey = "BATTLE_PET_DISPLAY_ID",
     },
@@ -465,48 +465,48 @@ RegisterSettingsTable(battlePetSettings, battlePets);
 local currencies = CreateSubcategory(L.CONFIG_HEADER_CURRENCY_TOOLTIPS);
 
 local currrencySettings = {
-    [1] = {
+    {
         Name = "TooltipCurrencyShowCurrencyID",
         LocKey = "CURRENCY_ID",
     },
-    [2] = {
+    {
         Name = "TooltipCurrencyShowIcon",
         LocKey = "CURRENCY_ICON",
     },
-    [3] = {
+    {
         Name = "TooltipCurrencyShowLimitedPerWeek",
         LocKey = "CURRENCY_LIMITED_PER_WEEK",
     },
-    [4] = {
+    {
         Name = "TooltipCurrencyShowTradeable",
         LocKey = "CURRENCY_IS_TRADEABLE",
     },
-    [5] = {
+    {
         Name = "TooltipCurrencyShowDiscovered",
         LocKey = "CURRENCY_DISCOVERED",
         Default = false,
     },
-    [6] = {
+    {
         Name = "TooltipCurrencyShowAccountWide",
         LocKey = "CURRENCY_IS_ACCOUNT_WIDE",
     },
-    [7] = {
+    {
         Name = "TooltipCurrencyShowTransferable",
         LocKey = "CURRENCY_CAN_XFER",
     },
-    [8] = {
+    {
         Name = "TooltipCurrencyShowTransferPercentage",
         LocKey = "CURRENCY_XFER_PERCENTAGE",
     },
-    [9] = {
+    {
         Name = "TooltipCurrencyShowAmountPerCycle",
         LocKey = "CURRENCY_AMOUNT_PER_CYCLE",
     },
-    [10] = {
+    {
         Name = "TooltipCurrencyShowCycleDuration",
         LocKey = "CURRENCY_CYCLE_DURATION",
     },
-    [11] = {
+    {
         Name = "TooltipCurrencyShowHasWarmodeBonus",
         LocKey = "CURRENCY_HAS_WARMODE_BONUS",
         Default = false,
@@ -520,7 +520,7 @@ RegisterSettingsTable(currrencySettings, currencies);
 local gobjects = CreateSubcategory(L.CONFIG_HEADER_GOBJECT_TOOLTIPS);
 
 local gobjectSettings = {
-    [1] = {
+    {
         Name = "TooltipObjectShowID",
         LocKey = "GOBJECT_ID",
     },
