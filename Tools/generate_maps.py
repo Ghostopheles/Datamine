20f098872cf58ce816bfcaa966df83db979c1698
Ghost <ghost@ghst.tools>
2026-04-30 21:39:47 -0500

    12.0.7.67227 maps

--- Tools/generate_maps.py
+++ Tools/generate_maps.py
@@ -12,7 +12,7 @@ ADDON_DIR = parent_directory / "Datamine_Maps"
 OUTPUT_DIR = ADDON_DIR / "Generated"
 
 WOW_DIR = "F:/Games/World of Warcraft"
-FLAVOR = "wow"
+FLAVOR = "wowxptr"
 
 TOC_FILE_LIST_LINE_NO = 19
 
